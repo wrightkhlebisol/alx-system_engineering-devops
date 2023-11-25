2a6b8f62319e94368117fe67df9424fe3672aeef
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-25 02:03:27 +0000

    fix lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a tmp file with Puppet
 
 file { '/tmp/school':
-    ensure => 'file',
+    ensure  => 'file',
     content => 'I love Puppet',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
