83896518e5a3849f3ba92ee05e7c0316ef922452
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-25 02:29:44 +0000

    add -f

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # Kills a process
 
 exec { 'kill_given_process':
-  command     => '/usr/bin/pkill killmenow',
+  command     => '/usr/bin/pkill -f killmenow',
   refreshonly => true,
-  onlyif      => '/usr/bin/pgrep killmenow',
+  onlyif      => '/usr/bin/pgrep -f killmenow',
 }
 
