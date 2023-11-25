d95caabd67b06b831bcd30f502138743001b001d
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-25 02:01:08 +0000

    fix issue

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a tmp file with Puppet
 
-file { '/tmp/school'
-    ensure => present,
+file { '/tmp/school':
+    ensure => 'file',
     content => 'I love Puppet',
     mode => '0744',
     owner => 'www-data',
