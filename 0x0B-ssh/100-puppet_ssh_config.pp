f4eda35a88a3b7755b7a754d113a6a8d203836d4
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-27 09:44:25 +0000

    create host config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,19 @@
 # Configure ssh_config
-$ssh_path = '/etc/ssh/sshd_config'
+#$ssh_path = '/etc/ssh/sshd_config'
 
-file_line { 'Turn off passwd auth':
-  path => $ssh_path,
-  line => 'PasswordAuthentication no',
+#file_line { 'Turn off passwd auth':
+#  path => $ssh_path,
+#  line => 'PasswordAuthentication no',
+#}
+
+#file_line { 'Declare identity file':
+#  path => $ssh_path,
+#  line => 'IdentityFile ~/.ssh/school',
+#}
+
+$host_config = '/root/.ssh/config'
+
+file { $host_config:
+  ensure  => 'present',
+  content => "PasswordAuthentication no\nIdentityFile school"
 }
-
-file_line { 'Declare identity file':
-  path => $ssh_path,
-  line => 'IdentityFile ~/.ssh/school',
-}
-
