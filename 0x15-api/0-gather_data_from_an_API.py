d054d2c6be626176bddcd0867611b3c1245b917e
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-03 01:25:01 +0000

    print response data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,6 +10,25 @@ if __name__ == "__main__":
     import sys
 
     userId = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/users/{userId}/todos'
-    response = requests.get(url)
-    print(response)
+    base_url = f'https://jsonplaceholder.typicode.com'
+
+    users_path = f'/users/{userId}'
+    users_url = f'{base_url}{users_path}'
+
+    completed_path = f'/users/{userId}/todos?completed=true'
+    incomplete_path = f'/users/{userId}/todos?completed=false'
+    completed_url = f'{base_url}{completed_path}'
+    incomplete_url = f'{base_url}{incomplete_path}'
+
+    user = requests.get(users_url).json()
+    completed = requests.get(completed_url).json()
+    incomplete = requests.get(incomplete_url).json()
+
+    len_c = len(completed)
+    diff = len_c + len(incomplete)
+    username = user.get('name')
+    print(f'''Employee {username} is done with tasks({len_c}/{diff}):''')
+
+    for c_task in completed:
+        print(f'\t {c_task.get("title")}')
+
